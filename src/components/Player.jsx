299d2c097225ce5156a8724856f491732169e833
Africa-t0t0 <jorge.ramirezz@mail.udp.cl>
2024-10-06 00:07:03 -0300

    using useRef

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,28 +1,46 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Player() {
 
+  // now we are using refs.
+  const playerName = useRef();
+
+
   // Using this approach, we use a lot of code, this can be more simple if we use ref!
   const [userName, setUserName] = useState('');
-  const [submitted, setSubmitted] = useState(false);
+  // const [submitted, setSubmitted] = useState(false);
 
-  function handleNameChange(event) {
-    setSubmitted(false);
-    setUserName(event.target.value);
-  }
+  // function handleNameChange(event) {
+  //   setSubmitted(false);
+  //   setUserName(event.target.value);
+  // }
 
   function handleClick() {
-    setSubmitted(true);
+    // setSubmitted(true);
+    // you will always need to access the current value!
+    // with REF, we are making a connection between the input and the ref, this allows us to access ALL THE VALUES of the dom element,
+    // in this case, the input tag.
+    setUserName(playerName.current.value);
+    // a way of clearing the input is this, but not the best case, we should always avoid manipulating the DOM trough Ref
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? userName : 'unknown entity'}</h2>
+      {/* <h2>Welcome {userName ? userName : 'unknown entity'}</h2> */}
+      {/* another way of writing thar code is */}
+      <h2>Welcome {userName ?? 'unknown entity'}</h2>
       <p>
-        <input
+        {/* old version */}
+        {/* <input
+          ref={playerName}
           type="text"
           onChange={handleNameChange}
           value={userName}
+        /> */}
+        {/* using Ref */}
+        <input
+          type="text"
+          ref={playerName}
         />
         <button
           onClick={handleClick}
